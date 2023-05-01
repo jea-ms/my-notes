0579ef3e6f5604673c690df412e424ef86b157b5
Jea Suson <susonjea@gmail.com>
2023-05-01 18:42:42 +0800

    ch 23: login route to verify email

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -62,10 +62,20 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  notesRoute,
-                  (route) => false,
-                );
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  // user's email is verified
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    notesRoute,
+                    (route) => false,
+                  );
+                } else {
+                  // user not verified
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    verifyEmailRoute,
+                    (route) => false,
+                  );
+                }
               } on FirebaseAuthException catch (e) {
                 devtools.log(e.code);
                 devtools.log(e.toString());
@@ -93,4 +103,3 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
-
